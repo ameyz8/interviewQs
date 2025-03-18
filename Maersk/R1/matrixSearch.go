4049be5fbd39f0d982a5236892c0874a7a14880c
ameyz7 <95076481+ameyz8@users.noreply.github.com>
2025-03-18 19:46:44 +0530

    main | added matrix search problem without answer

---
+++ Maersk/R1/matrixSearch.go
@@ -0,0 +1,17 @@
+/*
+
+Search in a row wise and column wise sorted matrix
+Last Updated : 19 Dec, 2024
+Given a matrix mat[][] and an integer x, the task is to check if x is present in mat[][] or not. Every row and column of the matrix is sorted in increasing order.
+
+Examples:
+
+Input: x = 62, mat[][] = [[3, 30, 38],
+                            [20, 52, 54],
+                            [35, 60, 69]]
+Output: false
+Explanation: 62 is not present in the matrix.
+
+*/
+
+package main
